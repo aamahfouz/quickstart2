ba70afba3070b8e4c57488477db259ead100c5eb
AAMAHFOUZ <aa.mahfouz@dataidtech.com>
2016-09-17 02:02:07 +0200

    home, tasks and delete tasks routes done

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+use App\Task;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+Route::post('/tasks',function(Request $request){
+
+});
+
+Route::delete('/tasks/{id}',function($id){
+
 });
